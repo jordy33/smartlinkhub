f31510d7c40e69797da98370a6c92bd09e8dea67
Jorge Macias <jorgemacias@hotmail.com>
2014-09-30 15:47:34 -0500

    time out control added

--- core/smartlinkhub.py
+++ core/smartlinkhub.py
@@ -18,44 +18,75 @@ for x in range (1,11):
     address.append(c.fetchone()[0]+" random")
 
 class service(socketserver.BaseRequestHandler):
+    def setup(self):
+        # the socket is called request in the request handler
+        self.request.settimeout(15.0)
     def handle(self):
         data = 'dummy'
         isConnected=False
         #print ("Client connected with: ", self.client_address)
         while len(data):
-            data = self.request.recv(1024)
+            try:
+                data = self.request.recv(1024)
+                if not data:
+                    break  # connection is closed
+                else:
+                    pass
+            except:
+                break
             if len(data)>0 and len(data)<5 and data[0]==99 and isConnected==False:  #Not null, its (C)onnect command, 4 len command
                 if data[1]>=48 and data[1]<=57:
                     try:
                         bleconn = Peripheral(address[data[1]-48])
                     except BTLEException as e:
-                        self.request.send(b'error connecting to device\r\n')
-                        isConnected=False
+                        try:
+                            self.request.send(b'error connecting to device\r\n')
+                            isConnected=False
+                        except:
+                            break
                     else:
                         isConnected=True
-                        self.request.send(b'connected\r\n')
-                        bleconn.writeCharacteristic(0x000f,binascii.a2b_hex("0100"))
+                        try:
+                            self.request.send(b'connected\r\n')
+                            bleconn.writeCharacteristic(0x000f,binascii.a2b_hex("0100"))
+                        except:
+                            break
             if len(data)!=0 and isConnected==True:
                 cmd=data.rstrip(b'\r\n')
-                if cmd!=b'' and cmd[0]!=99 and cmd[0]!=100:  #if command is not (c)onnect or (d)isconnect
+                if cmd!=b'' and cmd[0]!=99 and cmd[0]!=100 and cmd[0]>=97:  #if command is not (c)onnect or (d)isconnect 
                     try:
                         notify=bleconn.writeCharacteristicWn(0x0011,cmd,True)
                     except BTLEException as e:
                         isConnected=False
-                        self.request.send(b'error writing to device\r\n')
+                        try:
+                            self.request.send(b'error writing to device\r\n')
+                        except:
+                            break
                     else:
                         isConnected=True
-                        self.request.send(notify['d'][0])
-                        self.request.send(b'\r\n')
+                        try:
+                            self.request.send(notify['d'][0])
+                            self.request.send(b'\r\n')
+                        except:
+                            break
             if len(data)>0 and len(data)<5 and data[0]==100 and isConnected==True:  #
                 cmd=data.rstrip(b'\r\n')
                 if cmd!=b'':
-                    bleconn.disconnect()
-                    self.request.send(b'disconnected\r\n')
+                    try:
+                        bleconn.disconnect()
+                    except:
+                        break
+                    try:
+                        self.request.send(b'disconnected\r\n')
+                    except:
+                        break
                     isConnected=False
-        #print("Client exited")
-        if isConnected==True:
+        #print("Client exited or timed out")
+        #if isConnected==True:
+        try:
             bleconn.disconnect()
+        except:
+            pass
         self.request.close()
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
